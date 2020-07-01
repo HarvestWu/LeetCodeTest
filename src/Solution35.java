e07ea99d664788c7227689a9e3ff4d7e06de49e7
HarvestWu <harvestwu@std.uestc.edu.cn>
2020-07-01 14:16:21 +0800

    修改35

--- src/Solution35.java
+++ src/Solution35.java
@@ -11,4 +11,19 @@ public class Solution35 {
         }
         return nums.length;
     }
+
+    public int searchInsert1(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+        while(left <= right) {
+            int mid = (left + right) / 2;
+            if(nums[mid] == target) {
+                return mid;
+            } else if(nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return left;
+    }
 }
