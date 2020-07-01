d09baaaf3c9388ba1784fe2513d11833fa9a0607
HarvestWu <harvestwu@std.uestc.edu.cn>
2020-07-01 12:51:38 +0800

    修改26

--- src/Solution26.java
+++ src/Solution26.java
@@ -4,6 +4,9 @@
  **/
 public class Solution26 {
     public int removeDuplicates(int[] nums) {
+        if (nums.length == 0) {
+            return 0;
+        }
         int result = 0, len = nums.length;
         for (int i = 1; i < len; i++) {
             if (nums[i] != nums[result]) {
