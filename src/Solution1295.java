74ecf7380661817cf1f6df8e6b90c7bc67e6a8e8
HarvestWu <harvestwu@std.uestc.edu.cn>
2020-07-03 21:34:45 +0800

    增加1295解法

--- src/Solution1295.java
+++ src/Solution1295.java
@@ -13,4 +13,14 @@ public class Solution1295 {
         }
         return result;
     }
+
+    public int findNumbers1(int[] nums) {
+        int result = 0;
+        for (int t : nums) {
+            if ((int) (Math.log10(t) + 1) % 2 == 0) {
+                result++;
+            }
+        }
+        return result;
+    }
 }
