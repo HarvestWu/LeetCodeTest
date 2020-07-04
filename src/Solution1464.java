c8a6848a08395a7f9284d2e0d9ca08d01e9a9d7b
HarvestWu <harvestwu@std.uestc.edu.cn>
2020-07-04 21:29:33 +0800

    增加1464解法

--- src/Solution1464.java
+++ src/Solution1464.java
@@ -10,4 +10,16 @@ public class Solution1464 {
         int len = nums.length;
         return (nums[len - 1] - 1) * (nums[len - 2] - 1);
     }
+
+    public int maxProduct1(int[] nums) {
+        int a = nums[0], b = nums[1];
+        for (int i = 2; i < nums.length; i++) {
+            if (a > b) {
+                b = nums[i] > b ? nums[i] : b;
+            } else {
+                a = nums[i] > a ? nums[i] : a;
+            }
+        }
+        return (a - 1) * (b - 1);
+    }
 }
